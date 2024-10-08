aa78a4499c061e416036a91c03547bc1fb1d2007
DeniseRei <lopesdeniserei@gmail.com>
2024-10-08 19:16:56 -0300

    criação do Model Lesson, Controller, Migrate; criação do LessonResourcce, LessonRepository e rota

--- routes/api.php
+++ routes/api.php
@@ -2,15 +2,22 @@
 
 use App\Http\Controllers\Api\{
     CourseController,
-    ModuleController
+    ModuleController,
+    LessonController,
 };
+
 use Illuminate\Support\Facades\Route;
 
 //Courses
 Route::get('courses',[CourseController::class, 'index']);
 Route::get('courses/{id}',[CourseController::class, 'show']);
 
+//Modulos dos Cursos
 Route::get('courses/{id}/modules',[ModuleController::class, 'index']);
+
+//Modulos das Aulas
+Route::get('modules/{id}/lessons',[LessonController::class, 'index']);
+Route::get('lessons/{id}',[LessonController::class, 'show']);
 
 Route::get('/', function() {
     return response()->json([
